e1641ecf68b84e86333fc34a900450060e1673f2
João Gonçalves <joaovitordutrag@gmail.com>
2020-06-07 12:11:20 -0300

    Fixing mysql module

--- knexfile.ts
+++ knexfile.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 module.exports = {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
         host : process.env.DB_HOST,
         user : process.env.DB_USER,
