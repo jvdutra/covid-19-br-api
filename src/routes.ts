158bf5f06fa7c00194f1eec2bcec49d8c6673d19
João Gonçalves <joaovitordutrag@gmail.com>
2020-06-07 23:46:37 -0300

    Added index route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import CovidController from './controllers/CovidController';
 
 const routes = express.Router();
 
 const covidController = new CovidController();
+
+routes.get('/', (request: Request, response: Response) => {
+    return response.json({
+        success: true,
+        message: 'Index page! Go to /get'
+    });
+});
 
 routes.get('/get', covidController.index);
 routes.get('/get/:uf', covidController.show);
