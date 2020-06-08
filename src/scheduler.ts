cf6dac707c0bf3bc11e96c81fc014f62e08081d9
João Gonçalves <joaovitordutrag@gmail.com>
2020-06-07 22:57:36 -0300

    Update scheduler.ts

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,4 +1,4 @@
 import schedule from 'node-schedule';
 import scrapper from './scrapper/index';
 
-schedule.scheduleJob({hour: 22, minute: 14}, scrapper);
+schedule.scheduleJob({hour: 22, minute: 0o0}, scrapper);
