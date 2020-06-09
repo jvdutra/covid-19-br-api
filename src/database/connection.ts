6924e4ba2a9449fa4e263184754d4756f81055d2
João Gonçalves <joaovitordutrag@gmail.com>
2020-06-09 15:19:44 -0300

    Database timezone changed

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,10 +5,11 @@ dotenv.config();
 const connection = knex({
     client: 'mysql2',
     connection: {
-      host : process.env.DB_HOST,
-      user : process.env.DB_USER,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB_DATABASE
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      timezone: process.env.TZ
     },
     useNullAsDefault: true
 });
